aaba86ea8ac8080ee5e5dec7be68d9baf3710265
yuma1217 <siamxlunadir@icloud.com>
2016-06-13 20:24:35 +0900

    keijiban1 kansei

---
+++ write_shin.php
@@ -0,0 +1,35 @@
+<?php
+//エラーが出て分からなくなりググって調べたエラーを調べるおまじない
+ini_set("display_errors", On);
+error_reporting(E_ALL);
+
+$name = $_POST['name'];
+$contents = $_POST['contents'];
+
+if($name == '' || $contents = ''){
+  header('Location: bbs_shin.php');
+  exit();
+}
+
+$dsn = 'mysql:host=localhost; dbname=keijiban; charset=utf8';
+$user = 'user';
+$password = 'tech-aca';
+
+try{
+  $db = new PDO($dsn, $user, $password);
+  $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  $stmt = $db->prepare("
+    INSERT INTO post (name, contents)
+    VALUES(:name, :contents)"
+  );
+  $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+  $stmt->bindParam(':contents', $contents, PDO::PARAM_STR);
+  $stmt->execute();
+
+  header('Location: http://localhost:8888/tech-aca/bbs_shin.php');
+  exit();
+
+}catch(PDOExeption $e){
+  die('エラー:'.$e->getMessage());
+}
+?>
